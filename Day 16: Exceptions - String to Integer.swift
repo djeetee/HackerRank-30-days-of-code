ad1e84d2c02bc996b4b98c411bc77eec527369d3
djeetee@users.noreply.github.com <djeetee@users.noreply.github.com>
2018-03-21 22:55:47 -0400

    First cut

---
+++ Day 16: Exceptions - String to Integer.swift
@@ -0,0 +1,33 @@
+// 
+
+import Foundation
+
+/*
+ * Define an ErrorType
+ */
+enum StringToIntTypecastingError: Error {
+    case BadString
+}
+
+/*
+ * If typecasting is not possible, throw exception, otherwise return the Integer value
+ */
+func stringToInt(inputString: String) throws -> Int {
+    // Write your code here
+    guard let intValue = Int(inputString) else { 
+        throw StringToIntTypecastingError.BadString 
+    }
+    
+    return intValue
+}
+
+/*
+ * Read the input
+ */
+let inputString = readLine()!
+
+do {
+    try print(stringToInt(inputString: inputString))
+} catch StringToIntTypecastingError.BadString {
+    print("Bad String")
+}

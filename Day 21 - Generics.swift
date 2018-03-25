ee39ac18f39db61bff45be5ae30ab4079720ab42
djeetee@users.noreply.github.com <djeetee@users.noreply.github.com>
2018-03-25 15:34:19 -0400

    First Cut

---
+++ Day 21 - Generics.swift
@@ -0,0 +1,35 @@
+// https://www.hackerrank.com/challenges/30-generics/problem
+
+struct Printer<T> {
+	/**
+	*    Name: printArray
+	*    Print each element of the generic array on a new line. Do not return anything.
+	*    @param A generic array
+	**/
+	
+	// Write your code here
+    
+    // the printArray() takes any array and prints its elements
+    func printArray(array: [T]) -> Void {
+        for item in array {
+            print(item)
+        }
+    }
+}
+
+var n = Int(readLine()!)!
+
+var intArray = Array(repeating: 0, count: n)
+for i in 0...n - 1 {
+	intArray[i] = Int(readLine()!)!
+}
+
+n = Int(readLine()!)!
+
+var stringArray = Array(repeating: "", count: n)
+for i in 0...n - 1 {
+	stringArray[i] = readLine()!
+}
+
+Printer<Int>().printArray(array: intArray)
+Printer<String>().printArray(array: stringArray)
